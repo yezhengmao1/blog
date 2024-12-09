793b3fd3166317d29c61e91f8511784be2abcaac
yezhengmao <yezhengmaolove@gmail.com>
2024-12-09 14:26:43 +0800

    [publications] delete the space

--- src/pages/publication.js
+++ src/pages/publication.js
@@ -18,19 +18,14 @@ export default function Publication() {
                         <strong>Zhengmao Ye</strong>, Dengchun Li, Zetao Hu, Tingfen Lan, Sha Jian, Sicong Zheng, Lei Duan, Jie Zuo, Hui Lu, Yuanchun Zhou, Mingjie Tang
                         <br></br>
                         under revision <i>Proceedings of Very Large Data Bases Conference (VLDB), 2025.</i>
-                        <img alt="Static Badge" src="https://img.shields.io/badge/VLDB-blue" style={{ marginLeft: '5px' }}></img>
                     </li>
 
                     <li className={styles.publications}>
-                        <span style={{ fontSize: '16px', lineHeight: 1, marginRight: '5px' }}>
-                        </span>
-
                         <b>Diffusion Counterfactual-Based Anomaly Detection in Class-Imbalanced Data</b>
                         <br></br>
                         Xinyun Shen, Min Li, <strong>Zhengmao Ye</strong>, Zhenyang Yu, Lei Duan
                         <br></br>
                         under rebuttal <i>IEEE International Conference on Acoustics, Speech, and Signal Processing (ICASSP), 2025.</i>
-                        <img alt="Static Badge" src="https://img.shields.io/badge/ICASSP-blue" style={{ marginLeft: '5px' }}></img>
                     </li>
 
                     <hr className={styles.pub_years}></hr>
@@ -43,7 +38,6 @@ export default function Publication() {
                         Dengchun Li, Yingzi Ma, Naizheng Wang, <strong>Zhengmao Ye</strong>, Zhiyuan Cheng, Yinghao Tang, Yan Zhang, Lei Duan, Jie Zuo, Cal Yang, Mingjie Tang
                         <br></br>
                         arXiv preprint arXiv:2404.15159, 2024
-                        <img alt="Static Badge" src="https://img.shields.io/badge/arXiv-red" style={{ marginLeft: '5px' }}></img>
                     </li>
                 </ul>
             </div>
